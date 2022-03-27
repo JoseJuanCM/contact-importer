8daa728704244f39b30e476ab9a7beda802aa9b5
Juan De la Cruz <juan.cruz.mejia@gmail.com>
2022-03-27 11:51:03 -0600

    feat(contacts): app created

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,7 +79,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "contact_importer.users",
-    # Your stuff: custom apps go here
+    "contact_importer.contacts.apps.ContactsConfig"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ contact_importer/contacts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContactsConfig(AppConfig):
+    name = 'contact_importer.contacts'
+    verbose_name = "Contacts"
