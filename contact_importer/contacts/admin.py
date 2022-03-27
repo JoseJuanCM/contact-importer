0c87adf3a72e1d992161ce464d9d7d8cf041233f
Juan De la Cruz <juan.cruz.mejia@gmail.com>
2022-03-27 17:47:53 -0600

    feat(django_admin): file models added

--- contact_importer/contacts/admin.py
+++ contact_importer/contacts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact_importer.contacts.models import FileImport, FileImportErrors, Contact
+
+admin.site.register(FileImport)
+admin.site.register(FileImportErrors)
+admin.site.register(Contact)
