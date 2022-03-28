c0dcdc598b344177d45885eb5b20125c12af1030
Juan De la Cruz <juan.cruz.mejia@gmail.com>
2022-03-28 01:11:07 -0600

    feat(endpoints): added

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from contact_importer.contacts.api.views import FileImportViewSet
+from contact_importer.contacts.api.views import FileImportViewSet, ContactViewSet, FileImportErrorsViewSet
 from contact_importer.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -11,6 +11,8 @@ else:
 
 router.register("users", UserViewSet)
 router.register("files", FileImportViewSet)
+router.register("contacts", ContactViewSet)
+router.register("file-errors", FileImportErrorsViewSet)
 
 
 app_name = "api"
